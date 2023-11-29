231469593fa933de8086c1413a402aa8003d19c4
yagami <tanjipon@gmail.com>
2023-11-29 22:42:08 +0800

    Optimize scroll effect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 import './App.css'
 
@@ -30,6 +30,8 @@ import { animated, useSpring } from '@react-spring/web';
 
 import { TypeAnimation } from 'react-type-animation';
 
+import { useInViewEffect } from 'react-hook-inview'
+
 function App() {
   const queryParameters: URLSearchParams = new URLSearchParams(window.location.search);
   const receiver: string | null = queryParameters.get("r");
@@ -38,25 +40,100 @@ function App() {
 
   const audioPlayer = useRef<HTMLAudioElement>(null); 
 
+  // const [letterRef, isVisible] = useInView({
+  //   threshold: 0.1,
+  // });
+
+  const [currPage, setCurrPage] = useState(0);
+  const p0Ref = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 0) {
+        parallax.current.scrollTo(0);
+        setCurrPage(0);
+      }
+    },
+    { threshold: 0 },
+  );
+
+  const p1Ref = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 1) {
+        parallax.current.scrollTo(1);
+        setCurrPage(1);
+      }
+    },
+    { threshold: 0 },
+  );
+
+  const p2TRef = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 2) {
+        parallax.current.scrollTo(2);
+        setCurrPage(2);
+      }
+    },
+    { threshold: 0 },
+  );
+
+  const p2BRef = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 2) {
+        parallax.current.scrollTo(2);
+        setCurrPage(2);
+      }
+    },
+    { threshold: 0 },
+  );
+
+  const p3TRef = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 3) {
+        parallax.current.scrollTo(3);
+        setCurrPage(3);
+      }
+    },
+    { threshold: 0 },
+  );
+
+  const p4Ref = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 4) {
+        parallax.current.scrollTo(4);
+        setCurrPage(4);
+      }
+    },
+    { threshold: 0 },
+  );
+
+  const p5Ref = useInViewEffect(
+    ([entry]) => {
+      if (entry.isIntersecting && currPage !== 5) {
+        parallax.current.scrollTo(5);
+        setCurrPage(5);
+      }
+    },
+    { threshold: 0 },
+  );
+
   // const [currPage, setCurrPage] = useState(0);
   function getWelcomLetter(){
-    // if (currentPage === 1){
-    return (
-      <TypeAnimation
-        className='w-full h-full text-center text-xl lg:text-3xl md:text-xl sm:text-xl'
-        sequence={[
-          // Same substring at the start will only be typed once, initially
-          getLetterText(receiver),
-        ]}
-        speed={10}
-        style={{ whiteSpace: 'pre-line', color: '#3D464E', fontFamily: 'chenyuluoyan-mono' }}
-        cursor={false}
-        wrapper='div'    
-      />
-    );
-    // } else {
-    //   return <></>;
-    // }
+    if (currPage === 1){
+      return (
+        <TypeAnimation
+          className='w-full h-full text-center text-xl lg:text-3xl md:text-xl sm:text-xl'
+          sequence={[
+            // Same substring at the start will only be typed once, initially
+            getLetterText(receiver),
+          ]}
+          speed={10}
+          style={{ whiteSpace: 'pre-line', color: '#3D464E', fontFamily: 'chenyuluoyan-mono' }}
+          cursor={false}
+          wrapper='div'    
+        />
+      );
+    } else {
+      return <></>;
+    }
   }
 
   function getLetterText(receiver: string | null): string {
@@ -69,59 +146,6 @@ function App() {
     });
     return txt;
   }
-
-  // const [lastOffset, setLastOffset] = useState(0);
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const pageHeight: number = parallax.current.space;
-  //     const currentOffset: number = parallax.current.current / pageHeight;
-  //     const currentPage: number = Math.trunc(currentOffset);
-  //     const currentPageOffset: number = currentOffset - currentPage;
-
-  //     if (currentOffset > lastOffset && currentPageOffset >= 0.1){
-  //       parallax.current.scrollTo(Math.ceil(currentOffset));
-  //       setCurrPage(Math.ceil(currentOffset));
-  //     } else if (currentOffset < lastOffset && currentPageOffset <= 0.9){
-  //       parallax.current.scrollTo(Math.floor(currentOffset));
-  //       setCurrPage(Math.floor(currentOffset));
-  //     }
-      
-  //     setLastOffset(currentOffset);
-  //   }
-
-  //   const container = parallax.current.container.current;
-  //   container.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     container.removeEventListener('scroll', handleScroll);
-  //   }
-  // }, []);
-
-  // const handleScroll = () => {
-  //   const pageHeight: number = parallax.current.space;
-  //   const currentOffset: number = parallax.current.current / pageHeight;
-  //   const currentPage: number = Math.trunc(currentOffset);
-  //   const currentPageOffset: number = currentOffset - currentPage;
-
-  //   if (currentOffset > lastOffset && currentPageOffset >= 0.1){
-  //     parallax.current.scrollTo(Math.ceil(currentOffset));
-  //     setCurrPage(Math.ceil(currentOffset));
-  //   } else if (currentOffset < lastOffset && currentPageOffset <= 0.9){
-  //     parallax.current.scrollTo(Math.floor(currentOffset));
-  //     setCurrPage(Math.floor(currentOffset));
-  //   }
-    
-  //   setLastOffset(currentOffset);
-  // };
-
-  // useEffect(() => {
-  //   const container = parallax.current.container.current;
-  //   container.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     container.removeEventListener('scroll', handleScroll);
-  //   }
-  // }, []);
 
   window.addEventListener('click', () => {
     audioPlayer.current?.play();
@@ -214,12 +238,13 @@ function App() {
             <img className='w-full' src={photo1} />
           </animated.div>
           <animated.div 
+            ref={p0Ref} 
             className='absolute bg-white p-1 rounded w-4/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl' 
             style={p2Springs}
           >
             <img className='w-full' src={photo2} />
           </animated.div>
-          <animated.div 
+          <animated.div
             className='absolute bg-white p-1 rounded w-6/12 lg:w-3/12 md:w-4/12 sm:w-4/12 shadow-xl' 
             style={p3Springs}
           >
@@ -231,13 +256,13 @@ function App() {
             <div className='flex justify-center w-full'>
               <img
                   src={coverTaichiAndSsuYuText}
-                  width={'65%'}
+                  width={'70%'}
               />
             </div>
             <div className='flex justify-center w-full'>
               <img
                   src={coverWeddingText}
-                  width={'30%'}
+                  width={'35%'}
               />
             </div>
           </div>
@@ -251,16 +276,16 @@ function App() {
             </div>
         </ParallaxLayer>
         <ParallaxLayer className='flex justify-center items-center' offset={1} speed={0}>
-          <div className='w-11/12 lg:w-6/12 md:w-10/12 sm:w-10/12 h-4/6 bg-no-repeat bg-center bg-contain' style={{ backgroundImage: `url(${letterBgImg})` }}>
+          <div ref={p1Ref} className='w-11/12 lg:w-6/12 md:w-10/12 sm:w-10/12 h-4/6 bg-no-repeat bg-center bg-contain' style={{ backgroundImage: `url(${letterBgImg})` }}>
             <div className='p-4 shadow-lg rounded-md w-full h-full' style={{ backgroundColor: 'rgba(255,255,255,0.8)' }}>
               { getWelcomLetter() }
             </div>
           </div>
         </ParallaxLayer>
         <ParallaxLayer className='relative' offset={2} speed={0}>
-          <div className='absolute flex flex-row justify-center items-center w-full left-1/20 lg:-left-1/4 md:-left-1/4 sm:-left-1/4' style={{ top: '18%' }}>
+          <div ref={p2TRef} className='absolute flex flex-row justify-center items-center w-full left-1/20 lg:-left-1/4 md:-left-1/4 sm:-left-1/4' style={{ top: '18%' }}>
             <div 
-              className='bg-white p-1 rounded w-4/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl'
+              className='bg-white p-1 rounded w-5/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl'
               style={{ transform: 'rotate(-3deg)' }} 
             >
               <img className='w-full' src={ssuyuSingle} />
@@ -271,14 +296,14 @@ function App() {
               <div>游象銘 高玉娟</div>
             </div>
           </div>
-          <div className='absolute flex flex-row justify-center items-center w-full right-1/20 lg:-right-1/4 md:-right-1/4 sm:-right-1/4' style={{ bottom: '18%' }}>
+          <div ref={p2BRef} className='absolute flex flex-row justify-center items-center w-full right-1/20 lg:-right-1/4 md:-right-1/4 sm:-right-1/4' style={{ bottom: '18%' }}>
             <div className='flex flex-col pr-4 text-2xl lg:text-5xl md:text-4xl sm:text-3xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
               <div>新郎 彭泰淇</div>
               <div className='mt-7'>新郎家長</div>
               <div>彭文良 賴儀娟</div>
             </div>
             <div 
-              className='bg-white p-1 rounded w-4/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl' 
+              className='bg-white p-1 rounded w-5/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl' 
               style={{ transform: 'rotate(5deg)' }}
             >
               <img className='w-full' src={taichiSingle} />
@@ -286,7 +311,7 @@ function App() {
           </div>
         </ParallaxLayer>
         <ParallaxLayer className='flex flex-col justify-center items-center' offset={3} speed={0}>
-          <div style={{ fontSize: '3em', fontFamily: 'chenyuluoyan-mono', color: '#3D464E', fontWeight: 'bolder' }}>
+          <div ref={p3TRef} style={{ fontSize: '3em', fontFamily: 'chenyuluoyan-mono', color: '#3D464E', fontWeight: 'bolder' }}>
             <p>婚宴資訊</p>
           </div>
           <div className='text-3xl lg:text-3xl md:text-3xl sm:text-2xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
@@ -306,15 +331,15 @@ function App() {
           </div>
         </ParallaxLayer>
         <ParallaxLayer className='flex flex-col justify-center items-center' offset={4} speed={0}>
-          <div className='p-1 shadow-md w-11/12 lg:w-4/12 md:w-5/12 sm:w-7/12' style={{ backgroundColor: 'white'}}>
-            <img
-              className='w-full'
-              src={dazhidenhuaTrans}
-            />
-          </div>
+            <div ref={p4Ref} className='p-1 shadow-md w-11/12 lg:w-4/12 md:w-5/12 sm:w-7/12' style={{ backgroundColor: 'white'}}>
+              <img
+                className='w-full'
+                src={dazhidenhuaTrans}
+              />
+            </div>
         </ParallaxLayer>
         <ParallaxLayer className='flex flex-col justify-center items-center' offset={5} speed={0}>
-         <div className='mb-4 flex justify-center w-11/12 lg:w-4/12 md:w-4/12 sm:w-4/12'>
+         <div ref={p5Ref} className='mb-4 flex justify-center w-11/12 lg:w-4/12 md:w-4/12 sm:w-4/12'>
               <img
                   className='w-full'
                   src={seeYouOn0128}
