e41101d35ea5b0bf24ccee61e4f9b6d6575f4065
yagami <tanjipon@gmail.com>
2023-12-05 00:11:21 +0800

    1. Optimize resource quality 2. Optimize animation performance 3. Disbale autoplay BGM 4. Adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,6 @@ import welcomeLetterText from './assets/welcom-letter.json'
 import qSsuyu from './assets/ssuyu-q.webp';
 import qTaichi from './assets/taichi-q.webp';
 
-import { animated, useSpring } from '@react-spring/web';
-
 import { TypeAnimation } from 'react-type-animation';
 
 import { useInView } from 'react-hook-inview'
@@ -47,12 +45,20 @@ function App() {
     setIsAudioPlaying(!isAudioPlaying);
   };
 
-  const [p1Ref, isP1Visible] = useInView({
+  const [letterRef, isLetterVisible] = useInView({
+    threshold: 0,
+  });
+
+  const [taichiSingleRef, isTaichiSingleVisible] = useInView({
+    threshold: 0,
+  });
+
+  const [ssuyuSingleRef, isSsuYuSingleVisible] = useInView({
     threshold: 0,
   });
 
   function getWelcomLetter(){
-    if (isP1Visible){
+    if (isLetterVisible){
       return (
         <TypeAnimation
           className='w-full h-full text-center text-xl lg:text-3xl md:text-xl sm:text-xl'
@@ -90,127 +96,71 @@ function App() {
   }, [isAudioPlaying]);
 
   useEffect(() => {
-    const handleBgmPlay = () => {
-      if (!isAudioPlaying){
-        audioPlayer.current?.play();
-        setIsAudioPlaying(!isAudioPlaying);
-        if (audioPlayer.current){
-          audioPlayer.current.volume = 0.3;
-        }
-      }
-    };
-    window.addEventListener('click', handleBgmPlay, {once: true});
-    window.addEventListener('touchstart', handleBgmPlay, {once: true});
-    return () => {
-      window.removeEventListener('click', handleBgmPlay);
-      window.removeEventListener('touchstart', handleBgmPlay);
-    };
+    // const handleBgmPlay = () => {
+    //   if (!isAudioPlaying){
+    //     audioPlayer.current?.play();
+    //     setIsAudioPlaying(!isAudioPlaying);
+    //     if (audioPlayer.current){
+    //       audioPlayer.current.volume = 0.1;
+    //     }
+    //   }
+    // };
+    if (audioPlayer.current){
+      audioPlayer.current.volume = 0.1;
+    }
+    // window.addEventListener('click', handleBgmPlay, {once: true});
+    // window.addEventListener('touchstart', handleBgmPlay, {once: true});
+    // return () => {
+    //   window.removeEventListener('click', handleBgmPlay);
+    //   window.removeEventListener('touchstart', handleBgmPlay);
+    // };
   }, []);
-
-  const [p1Springs] = useSpring(
-    () => ({
-      from: {
-        top: '52.5%',
-        left: '-2%',
-        opacity: '0',
-        transform: 'rotate(9deg)',
-        filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))',
-      },
-      to: { 
-        top: '42.5%',
-        left: '-2%',
-        opacity: '1',
-        transform: 'rotate(9deg))',
-        filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))',
-      },
-      config: {
-        duration: 1250,
-      },
-    }),
-    []
-  );
-
-  const [p2Springs] = useSpring(
-    () => ({
-      from: {
-        top: '50%',
-        right: '2%',
-        opacity: '0',
-        transform: 'rotate(-15deg)',
-        filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))'  
-      },
-      to: { 
-        top: '40%',
-        right: '2%',
-        opacity: '1',
-        transform: 'rotate(-15deg)',
-        filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))'
-      },
-      delay: 250,
-      config: {
-        duration: 1250
-      },
-    }),
-    []
-  );
-
-  const [p3Springs] = useSpring(
-    () => ({
-      from: {
-        top: '63%',
-        right: '20%',
-        opacity: '0',
-        transform: 'rotate(-8deg)',
-        filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))'  
-      },
-      to: { 
-        top: '53%',
-        right: '20%',
-        opacity: '1',
-        transform: 'rotate(-8deg)',
-        filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))'
-      },
-      delay: 500,
-      config: {
-        duration: 1250
-      },
-    }),
-    []
-  );
 
   return (
     <>
-      <audio ref={audioPlayer} src={bgm} loop />
-      <Parallax className='bg-no-repeat bg-center bg-cover' ref={parallax} pages={6} style={{ top: '0', left: '0', backgroundImage: `url(${bgImg})`, backgroundColor: '#3D464E' }}>
+      <audio ref={audioPlayer} src={bgm} loop/>
+      <Parallax className='bg-no-repeat bg-center bg-cover' ref={parallax} pages={8} style={{ top: '0', left: '0', backgroundImage: `url(${bgImg})`, backgroundColor: '#3D464E' }}>
         <ParallaxLayer className='relative' offset={0} speed={0}>
-          <animated.div
-            className='absolute bg-white p-1 rounded w-6/12 lg:w-3/12 md:w-4/12 sm:w-4/12 shadow-xl' 
-            style={p1Springs}
+          <div
+            className='absolute w-6/12 lg:w-3/12 md:w-4/12 sm:w-4/12 animate-fade-up animate-once animate-duration-[1500ms] animate-delay-[50ms] animate-ease-in-out' 
+            style={{ 
+              top: '42.5%',
+              left: '-2%',
+              backgroundColor: 'background-color:rgba(0, 0, 0, 0)'
+            }}
           >
-            <img className='w-full' src={photo1} />
-          </animated.div>
-          <animated.div 
-            className='absolute bg-white p-1 rounded w-4/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl' 
-            style={p2Springs}
+            <img className='w-full p-1 rounded shadow-xl rotate-[9deg]' style={{ filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))', backgroundColor: '#FFFAFA' }} src={photo1}/>
+          </div>
+          <div 
+            className='absolute w-4/12 lg:w-2/12 md:w-3/12 sm:w-3/12 animate-fade-up animate-once animate-duration-[1500ms] animate-delay-[250ms] animate-ease-in-out' 
+            style={{ 
+              top: '40%',
+              right: '2%',
+              backgroundColor: 'background-color:rgba(0, 0, 0, 0)'
+            }}
           >
-            <img className='w-full' src={photo2} />
-          </animated.div>
-          <animated.div
-            className='absolute bg-white p-1 rounded w-6/12 lg:w-3/12 md:w-4/12 sm:w-4/12 shadow-xl' 
-            style={p3Springs}
+            <img className='w-full p-1 rounded shadow-xl rotate-[-15deg]' style={{ filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))', backgroundColor: '#FFFAFA' }} src={photo2}/>
+          </div>
+          <div
+            className='absolute w-6/12 lg:w-3/12 md:w-4/12 sm:w-4/12 animate-fade-up animate-once animate-duration-[1500ms] animate-delay-[500ms] animate-ease-in-out' 
+            style={{ 
+              top: '53%',
+              right: '20%',
+              backgroundColor: 'background-color:rgba(0, 0, 0, 0)'
+            }}
           >
-            <img className='w-full' src={photo3} />
-          </animated.div>
+            <img className='w-full p-1 rounded shadow-xl rotate-[-8deg]' style={{ filter: 'drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06))', backgroundColor: '#FFFAFA' }} src={photo3}/>
+          </div>
         </ParallaxLayer>
         <ParallaxLayer offset={0.15} speed={-0.2}>
           <div className='w-full'>
-            <div className='flex justify-center w-full'>
+            <div className='flex justify-center w-full animate-fade-down animate-once animate-duration-[1500ms] animate-delay-[50ms] animate-ease-in-out'>
               <img
                   src={coverTaichiAndSsuYuText}
                   width={'70%'}
               />
             </div>
-            <div className='flex justify-center w-full'>
+            <div className='flex justify-center w-full animate-fade-down animate-once animate-duration-[1500ms] animate-delay-[50ms] animate-ease-in-out'>
               <img
                   src={coverWeddingText}
                   width={'35%'}
@@ -218,7 +168,7 @@ function App() {
             </div>
           </div>
         </ParallaxLayer>
-        <ParallaxLayer className='relative w-full' sticky={{ start: 1, end: 5 }} speed={0}>
+        <ParallaxLayer className='relative w-full' sticky={{ start: 1, end: 7 }} speed={0}>
             <div className='absolute w-5/12 lg:w-2/12 md:w-4/12 sm:w-4/12 flex justify-center items-center animate-wiggle-more animate-infinite animate-duration-[2500ms] animate-delay-500 animate-ease-in' style={{ bottom: '4px', left: '-40px' }}>
               <img className='w-full' src={qSsuyu} />
             </div>
@@ -227,69 +177,101 @@ function App() {
             </div>
         </ParallaxLayer>
         <ParallaxLayer className='flex justify-center items-center' offset={1} speed={0}>
-          <div ref={p1Ref} className='w-11/12 lg:w-6/12 md:w-10/12 sm:w-10/12 h-4/6 bg-no-repeat bg-center bg-contain' style={{ backgroundImage: `url(${letterBgImg})` }}>
+          <div ref={letterRef} className='w-11/12 lg:w-6/12 md:w-10/12 sm:w-10/12 h-4/6 bg-no-repeat bg-center bg-contain' style={{ backgroundImage: `url(${letterBgImg})` }}>
             <div className='p-4 shadow-lg rounded-md w-full h-full' style={{ backgroundColor: 'rgba(255,255,255,0.8)' }}>
               { getWelcomLetter() }
             </div>
           </div>
         </ParallaxLayer>
-        <ParallaxLayer className='relative' offset={2} speed={0.2}>
-          <div className='absolute flex flex-row justify-center items-center w-full left-1/20 lg:-left-1/4 md:-left-1/4 sm:-left-1/4' style={{ top: '18%' }}>
-            <div 
-              className='bg-white p-1 rounded w-5/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl'
-              style={{ transform: 'rotate(-3deg)' }} 
-            >
-              <img className='w-full' src={ssuyuSingle} />
-            </div>
-            <div className='flex flex-col self-start pl-4 text-2xl lg:text-5xl md:text-4xl sm:text-3xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
-              <div>新娘 游思愉</div>
-              <div className='mt-3'>新娘家長</div>
-              <div>游象銘 高玉娟</div>
-            </div>
-          </div>
-          <div className='absolute flex flex-row justify-center items-center w-full right-1/20 lg:-right-1/4 md:-right-1/4 sm:-right-1/4' style={{ bottom: '18%' }}>
-            <div className='flex flex-col self-end pr-4 text-2xl lg:text-5xl md:text-4xl sm:text-3xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
-              <div>新郎 彭泰淇</div>
-              <div className='mt-3'>新郎家長</div>
-              <div>彭文良 賴儀娟</div>
-            </div>
-            <div 
-              className='bg-white p-1 rounded w-5/12 lg:w-2/12 md:w-3/12 sm:w-3/12 shadow-xl' 
-              style={{ transform: 'rotate(5deg)' }}
-            >
-              <img className='w-full' src={taichiSingle} />
-            </div>
+        <ParallaxLayer className='flex flex-col justify-center items-center' offset={2} speed={0.2}>
+        <div ref={taichiSingleRef} className='w-full h-full flex flex-col justify-center items-center'>
+          {
+            isTaichiSingleVisible? 
+            <>
+              <div 
+                className='bg-white p-1 rounded w-10/12 lg:w-3/12 md:w-5/12 sm:w-5/12 shadow-xl animate-fade-left animate-once animate-duration-[1500ms] animate-ease-in-out' 
+                style={{ transform: 'rotate(0deg)' }}
+              >
+                <img className='w-full' src={taichiSingle} />
+              </div>
+              <div className='flex flex-col text-4xl lg:text-5xl md:text-4xl sm:text-3xl mt-4 animate-fade-right animate-once animate-duration-[1500ms] animate-ease-in-out' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+                <div>新郎 彭泰淇</div>
+              </div>
+            </>
+            :
+            <></>
+          }
           </div>
         </ParallaxLayer>
         <ParallaxLayer className='flex flex-col justify-center items-center' offset={3} speed={0.2}>
-          <div style={{ fontSize: '3em', fontFamily: 'chenyuluoyan-mono', color: '#3D464E', fontWeight: 'bolder' }}>
+          <div ref={ssuyuSingleRef} className='w-full h-full flex flex-col justify-center items-center'>
+            {
+              isSsuYuSingleVisible?
+              <>
+                <div 
+                  className='bg-white p-1 rounded w-10/12 lg:w-3/12 md:w-5/12 sm:w-5/12 shadow-xl animate-fade-right animate-once animate-duration-[1500ms] animate-ease-in-out'
+                  style={{ transform: 'rotate(0deg)' }} 
+                >
+                  <img className='w-full' src={ssuyuSingle} />
+                </div>
+                <div className='flex flex-col text-4xl lg:text-5xl md:text-4xl sm:text-3xl mt-4 animate-fade-left animate-once animate-duration-[1500ms] animate-ease-in-out' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+                  <div>新娘 游思愉</div>
+                </div>
+              </>
+              :
+              <></>
+            }
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer className='flex flex-col justify-center items-center' offset={4} speed={0.2}>
+          <div style={{ fontSize: '4em', fontFamily: 'chenyuluoyan-mono', color: '#3D464E', fontWeight: 'bolder' }}>
             <p>婚宴資訊</p>
           </div>
-          <div className='text-3xl lg:text-3xl md:text-3xl sm:text-2xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+          <div className='mt-12 flex flex-row justify-center items-center'>
+            <div className='flex flex-col justify-center items-center mr-4'>
+              <div className='mt-3 text-4xl lg:text-5xl md:text-5xl sm:text-5xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+                新郎家長
+              </div>
+              <div className='text-4xl lg:text-5xl md:text-5xl sm:text-5xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+                彭文良 賴儀娟
+              </div>
+            </div>
+            <div className='flex flex-col justify-center items-center ml-4'>
+              <div className='mt-3 text-4xl lg:text-5xl md:text-5xl sm:text-5xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+                新娘家長
+              </div>
+              <div className='text-4xl lg:text-5xl md:text-5xl sm:text-5xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+                游象銘 高玉娟
+              </div>
+            </div>
+          </div>
+          <div className='mt-12 text-3xl lg:text-4xl md:text-4xl sm:text-4xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
             <p>大直典華 6F 花田盛事</p>
           </div>
-          <div className='text-3xl lg:text-3xl md:text-3xl sm:text-2xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+          <div className='text-3xl lg:text-4xl md:text-4xl sm:text-4xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
             <p>台北市中山區植福路8號</p>
           </div>
-          <div className='mb-2 text-3xl lg:text-3xl md:text-3xl sm:text-2xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
+          <div className='mb-2 text-3xl lg:text-4xl md:text-4xl sm:text-4xl' style={{ fontFamily: 'chenyuluoyan-mono', color: '#3D464E' }}>
             <p>2023 .01 .28　11:50 入席</p>
           </div>
-          <div className='p-1 shadow-md w-10/12 lg:w-5/12 md:w-8/12 sm:w-9/12' style={{ backgroundColor: 'white'}}>
+        </ParallaxLayer>
+        <ParallaxLayer className='flex flex-col justify-center items-center' offset={5} speed={0.2}>
+          <div className='p-1 shadow-md w-11/12 lg:w-5/12 md:w-8/12 sm:w-9/12' style={{ backgroundColor: 'white'}}>
             <img
               className='w-full'
               src={dazhidenhuaLocation}
             />
           </div>
         </ParallaxLayer>
-        <ParallaxLayer className='flex flex-col justify-center items-center' offset={4} speed={0.2}>
-            <div className='p-1 shadow-md w-11/12 lg:w-4/12 md:w-5/12 sm:w-7/12' style={{ backgroundColor: 'white'}}>
+        <ParallaxLayer className='flex flex-col justify-center items-center' offset={6} speed={0.2}>
+            <div className='p-1 shadow-md w-11/12 lg:w-4/12 md:w-7/12 sm:w-8/12' style={{ backgroundColor: 'white'}}>
               <img
                 className='w-full'
                 src={dazhidenhuaTrans}
               />
             </div>
         </ParallaxLayer>
-        <ParallaxLayer className='flex flex-col justify-center items-center' offset={5} speed={0.2}>
+        <ParallaxLayer className='flex flex-col justify-center items-center' offset={7} speed={0.2}>
          <div className='mb-4 flex justify-center w-11/12 lg:w-4/12 md:w-4/12 sm:w-4/12'>
               <img
                   className='w-full'
@@ -303,7 +285,7 @@ function App() {
             />
           </div>
         </ParallaxLayer>
-        <ParallaxLayer className='relative w-full' sticky={{ start: 0, end: 5 }} speed={0}> 
+        <ParallaxLayer className='relative w-full' sticky={{ start: 0, end: 7 }} speed={0}> 
           {
             isAudioPlaying?
             <div className='absolute flex justify-center items-center rounded-full border-2 cursor-pointer animate-spin animate-infinite animate-duration-[2000ms] animate-ease-in-out' style={{ bottom: '15px', right: '20px', backgroundColor: '#FFC0CB', borderColor: '#FFFAFA' }} onClick={toggleAudioControl}>
